84c7a3359a9791b91c2a6a57a75449afef8f12f4
Julian McNair <julianwmcnair@gmail.com>
2023-06-13 22:55:42 -0700

    Followed the example assignment prompt. Copied intial comment to message box.     # make your code below     # print('Hello World')

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 def main():
 
-    ##############################
-    # make your code below
-    # print('Hello World')
-    ##############################
-
+    
+    print('Hello World')
+    print('Python Programming')
 
 if __name__ == '__main__':
     main()
